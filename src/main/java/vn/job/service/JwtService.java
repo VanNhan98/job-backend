9009138df6a534af0f27aea4fb9dc8e403d438e1
Mai Van Nhan <gplgarena@gmail.com>
2024-10-18 08:23:37 +0700

    create token with login email

--- src/main/java/vn/job/service/JwtService.java
+++ src/main/java/vn/job/service/JwtService.java
@@ -1,13 +1,54 @@
 package vn.job.service;
 
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class JwtService {
+
+    @Value("${jwt.expiryMinutes}")
+    private long expiryMinutes;
+
+    @Value("${jwt.expiryDay}")
+    private long expiryDay;
+
+    @Value("${jwt.accessKey}")
+    private String accessKey;
+
+    @Value("${jwt.refreshKey}")
+    private String refreshKey;
+
     public String generateToken(UserDetails user) {
-        // TODO xu li tao ra token
-        return "access-token";
+        return generateToken(new HashMap<>(), user);
     }
+
+
+    private String generateToken(Map<String, Object> claims, UserDetails user) {
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiryMinutes * 24 * 60 * 1000 * expiryDay))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getKey() {
+        byte[] bytes = Decoders.BASE64.decode(accessKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+
 }
